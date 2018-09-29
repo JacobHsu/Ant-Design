f80a6bbb5544bc8c02d7aa824634ddd0e2b1a785
jacobhsu <jacob.hsu.tw@gmail.com>
2018-09-29 13:16:30 +0800

    v-model=lang

--- vue-demo/ant-design-vue-worldcup/src/components/HelloWorld.vue
+++ vue-demo/ant-design-vue-worldcup/src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <div class="filter mb-3">
       <a-checkbox>顯示紅牌</a-checkbox>
       <a-checkbox>顯示黃牌</a-checkbox>
-      <a-radio-group defaultValue="0">
+      <a-radio-group defaultValue="0" v-model="lang">
         <a-radio-button value="0">繁體</a-radio-button>
         <a-radio-button value="1">简体</a-radio-button>
         <a-radio-button value="2">English</a-radio-button>
@@ -12,6 +12,9 @@
     </div>
     <div>
       <a-table :columns="columns" :dataSource="data" :pagination="false" size="middle">
+        <span slot="league" slot-scope="league">{{ league[lang] }}</span>
+        <span slot="home" slot-scope="home, record">{{ home[lang] }}</span>
+        <span slot="guest" slot-scope="guest, record"> {{ guest[lang] }}</span>
         <span slot="score" slot-scope="text, record">{{ record.homeScore }} - {{ record.guestScore }}</span>
         <span slot="halfScore" slot-scope="text, record">{{ record.homeHalfScore }} - {{ record.guestHalfScore }}</span>
       </a-table>
@@ -25,20 +28,24 @@ import reqwest from 'reqwest';
 
 const columns = [{
     title: '賽事',
-    dataIndex: 'league[1]',
+    dataIndex: 'league',
+    scopedSlots: { customRender: 'league' },
 }, {
     title: '時間',
     dataIndex: 'matchTime',
+    scopedSlots: { customRender: 'matchTime' },
 }, {
     title: '主隊',
-    dataIndex: 'home[1]',
+    dataIndex: 'home',
+    scopedSlots: { customRender: 'home' },
 }, {
     title: '全場比分',
     dataIndex: 'score',
     scopedSlots: { customRender: 'score' },
 }, {
     title: '客隊',
-    dataIndex: 'guest[1]',
+    dataIndex: 'guest',
+    scopedSlots: { customRender: 'guest' },
 }, {
     title: '半場比分',
     dataIndex: 'halfScore',
@@ -51,6 +58,7 @@ export default {
     return {
       data: [],
       columns,
+      lang: 0,
     }
   },
   mounted() {
